85306637c425f366a6d192fe86f0ec39e868e949
raximvodil <raximvodilrozbek@gmail.com>
2022-07-25 16:45:56 +0500

    feat: success!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracArr = bracketsConfig.map((currentValue) => currentValue.join(""));
+  for (let i = 0; i < bracArr.length; i++) {
+    if (str.includes(bracArr[i])) {
+      str = str.replace(bracArr[i], "");
+      console.log(str);
+      i = -1;
+    }
+  }
+  return !str;
+};
